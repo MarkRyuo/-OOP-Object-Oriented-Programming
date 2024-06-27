5d9610014c1670db77c12cd1ecef3a97856ae1e4
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-27 16:18:40 +0800

    update herE

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ class Animal() :
 
     def __init__(self, name_of_animal, age_of_animal):
         self.name_of_animal = name_of_animal 
-        self.age_of_animal = age_of_dog
+        self.age_of_animal = age_of_animal
         
 
 
