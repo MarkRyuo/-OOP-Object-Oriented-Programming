0b140f9e22b40610cd599d2667d3eb546f73e3b0
MarkRyuo <malupajhonmark3035@gmail.com>
2024-06-28 14:47:16 +0800

    update herE

--- main.py
+++ main.py
@@ -109,7 +109,8 @@ class Animal() :
         
             if age_of_Dog.isdigit() :
                 print(f"Your dog age is {age_of_Dog}")
-            
+            else :
+                print(f"")
 
 
 Dog_ = Animal() 
