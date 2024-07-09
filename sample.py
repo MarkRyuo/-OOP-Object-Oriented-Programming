e57ac5a5f0c116d6e80f7d5bb8f53638f4ea66d7
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-09 15:21:50 +0800

    update here

--- sample.py
+++ sample.py
@@ -11,7 +11,14 @@ class routine :
         self.open_social_media()
         self.open_games()
 
+    def open_social_media(self) :
+        pass
+
+    def open_games(self) :
+        pass
+
 
 class Jhonmark_morningroutine(routine) :
 
-        
+    def open_social_media(self) :
+        print()
