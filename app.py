93e6e0b9105d626bbf469145f03d9a54f23458c9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-11 09:30:03 +0800

    update herE

--- app.py
+++ app.py
@@ -28,10 +28,10 @@ def Log_in() : # * This block code is for log in only
             get_username = input("Enter your username: ")
             
             if get_username.isdigit() : # * isdigit check for all of get_username is a digit number 
-            if get_username :
+                if get_username :
                 print("Invalid username the content is number")
-        else : 
-            break 
+            else : 
+                break 
 
         return get_username
     
