310bbd80eaea522f605746d5de0223b12aff29ea
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-20 17:34:06 +0800

    update herE

--- OPP/Inheritance/Car.py
+++ OPP/Inheritance/Car.py
@@ -3,7 +3,7 @@ from Vehicle import Vehicle
 class Car(Vehicle) :
     
     def __init__(self, name, types):
-        super().__init__(name, types)
+        super().__init__(name, types) #* Calling superclass constructor (Which is the Vehicle) 
     
     def go(self):
         print(f"The car is go!, {self.name}")
