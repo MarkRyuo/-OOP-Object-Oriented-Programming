f7f2151992ba1e4bcdf8f51cac53de18bcb4ebce
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-20 21:32:45 +0800

    update herE

---
+++ OPP/main.py
@@ -0,0 +1,23 @@
+
+# ? 4 OOP Pillars
+# * Inheritance  
+# * Abstract 
+# * Polymorphism
+# * Encapsulation 
+
+
+# Todo create a simple Inheritance 
+
+class Person() :
+    
+    #* This is the Superclass
+    
+    def __init__(self,name,age) :
+        self.name = name 
+        self.age = age 
+    
+    def name(self) :
+        pass 
+    
+    def age(self) :
+        pass
